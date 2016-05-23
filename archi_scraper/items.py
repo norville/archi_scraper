25758419c65d997b47d32f48dca399bdc744e2ec
norville <ing.norville@gmail.com>
2016-05-24 00:35:32 +0200

    Scraper scaffolding
    http://doc.scrapy.org/en/latest/intro/tutorial.html

--- archi_scraper/items.py
+++ archi_scraper/items.py
@@ -1,14 +1,8 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy import Item, Field
 
 
-class ArchiScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ArchItem(Item):
+    name = Field()
+    surname = Field()
+    sid = Field()
+    address = Field()
--- archi_scraper/settings.py
+++ archi_scraper/settings.py
@@ -1,22 +1,10 @@
-# -*- coding: utf-8 -*-
-
-# Scrapy settings for archi_scraper project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-
 BOT_NAME = 'archi_scraper'
 
 SPIDER_MODULES = ['archi_scraper.spiders']
 NEWSPIDER_MODULE = 'archi_scraper.spiders'
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'archi_scraper (+http://www.yourdomain.com)'
+USER_AGENT = 'archi_scraper'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -64,9 +52,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'archi_scraper.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'archi_scraper.pipelines.AlboToCSVPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
@@ -83,8 +71,8 @@ ROBOTSTXT_OBEY = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_DIR = 'httpcache'
+HTTPCACHE_IGNORE_HTTP_CODES = []
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
